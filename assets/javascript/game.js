448a4f317618e8afc97e189cbfa87434f2b8224c
rmjones112 <rmjones112gmail.com>
2019-08-30 00:07:28 -0700

    changed background color

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,6 @@
 
-
+document.body.style.backgroundColor = "#ffe6ff" 
+  
 // set Var alphabet options
 var alphabet= ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
