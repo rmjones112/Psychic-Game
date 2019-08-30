c119e4e77cf5e74bf29dbeba31a192b9af1571e7
rmjones112 <rmjones112gmail.com>
2019-08-29 22:00:47 -0700

    added javascript and header, image

---
+++ assets/javascript/game.js
@@ -0,0 +1,56 @@
+// set Var alphabet options
+var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+// Setting at zero 
+var wins = 0;
+var losses = 0;
+var guessesLeft = 9;
+var letter = [];
+var allLetters = null;
+
+
+// picks number for comp , use math.floor 
+var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+//The querySelector() method returns the first element that matches a specified CSS selector(s) in the document
+function countGuessesLeft() {
+	document.querySelector("#guessesLeft").innerHTML = "Guesses Left: " + guessesLeft;
+}
+
+function farUserGuesses() {
+	document.querySelector("#letter").innerHTML = "Your Guesses so far: " + letter.join(' ');
+}
+
+countGuessesLeft();
+
+
+// set for when user presses a key 
+document.onkeyup = function(event) {
+	guessesLeft--;
+
+    //String.fromCharCode() method returns a string created from the specified sequence
+	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+
+	letter.push(userGuess);
+	countGuessesLeft();
+	farUserGuesses();
+
+    //if, if else or else can be used for W/L
+    //use .innerhtml to change the HTML content of a <p> element with id="demo":
+	if (userGuess === computerGuess){
+		wins++;
+		document.querySelector("#wins").innerHTML = "Wins: " + wins;
+		restart();
+	} 
+	else if (guessesLeft === 0) {
+		losses++;
+		document.querySelector("#lose").innerHTML = "Loses: " + losses;
+		restart();
+	}
+  };
+//restart option so that game will begin again 
+  var restart = function() {
+	guessesLeft = 9;
+	letter = [];
+	var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+}
